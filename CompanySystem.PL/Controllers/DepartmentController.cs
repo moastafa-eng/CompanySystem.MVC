91682cc410729283c2a5815dc7828cb2e20ab5a7
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-06-24 13:18:45 +0300

    End Of Create View-(Department Module)

--- CompanySystem.PL/Controllers/DepartmentController.cs
+++ CompanySystem.PL/Controllers/DepartmentController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanySystem.PL.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanySystem.PL.Controllers
 {
     public class DepartmentController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crate(CreateDepartmentDto  dto)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+
             return View();
         }
     }
---
+++ CompanySystem.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanySystem.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is required.")]
+        public string Code { get; set; } = null!;
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Start date is required.")]
+        public DateTime CreateAt { get; set; }
+    }
+}
